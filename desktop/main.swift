25eaf62507ff48a76933433f3dede24730db774b
Armin Briegel <ab@scriptingosx.com>
2018-09-06 15:12:20 +0200

    Initial Commit

---
+++ desktop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  desktop
+//
+//  Created by Armin Briegel on 2018-09-06.
+//  Copyright © 2018 Scripting OS X. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
