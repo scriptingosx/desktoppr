1cb61a8925cecc06e2878ddc42b92cc27b971052
Armin Briegel <ab@scriptingosx.com>
2019-09-12 10:44:33 +0200

    updated sample script
    see more info at https://scriptingosx.com/2019/09/get-current-user-in-shell-scripts-on-macos/

--- examples/setdesktop.sh
+++ examples/setdesktop.sh
@@ -1,32 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
 ##
 ## sets the desktop using `desktoppr`
 ##
 
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
 # set the path to the desktop image file here
 picturepath="/Library/Desktop Pictures/BoringBlueDesktop.png"
 
 
 # verify the image exists
-if [[ ! -f "$picturepath" ]]; then
+if [ ! -f "$picturepath" ]; then
     echo "no file at $picturepath, exiting"
     exit 1
 fi
 
 # verify that desktoppr is installed
 desktoppr="/usr/local/bin/desktoppr"
-if [[ ! -x "$desktoppr" ]]; then
+if [ ! -x "$desktoppr" ]; then
     echo "cannot find desktoppr at $desktoppr, exiting"
     exit 1
 fi
 
 # get the current user
-loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 
 # test if a user is logged in
-if [[ $loggedInUser != "" ]]; then
+if [ -n $loggedInUser ]; then
     # get the uid
     uid=$(id -u "$loggedInUser")
     # do what you need to do
